9f9b1d2e0830042cbd121991758ebbe02852f0f6
Daniel Fain <daniel@fain.dev>
2025-01-19 19:07:04 -0500

    handle signals and errors gracefully in daemon

--- cmd/schedulerd/main.go
+++ cmd/schedulerd/main.go
@@ -1,5 +1,47 @@
 package main
 
+import (
+	"io/fs"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+	"task-scheduler/internal/daemon"
+)
+
 func main() {
-	// TODO: Init daemon server
+	logFile, err := os.OpenFile("/tmp/taskd.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, fs.ModeSocket)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer logFile.Close()
+	log.SetOutput(logFile)
+
+	server, err := daemon.NewServer()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = server.Start()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
+
+	errChan := make(chan error, 1)
+	go func() {
+		errChan <- server.Start()
+	}()
+
+	select {
+	case err := <-errChan:
+		log.Printf("Server stopped with error: %v", err)
+	case sig := <-sigChan:
+		log.Printf("Received signal: %v", sig)
+		if err := server.Shutdown(); err != nil {
+			log.Printf("Error during shutdown: %v", err)
+		}
+	}
 }
